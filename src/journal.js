474f2dbf153afaf9aff215af030a60e224454f79
Brandan Sayarath and Geoff Goetz <geoff@>
2020-02-03 16:36:34 -0800

    Add JS, gitignore and package.json

--- src/journal.js
+++ src/journal.js
@@ -1,15 +1,6 @@
-export function pingPong(goal) {
-  var output = [];
-  for (var i = 1; i <= goal; i++) {
-    if (i % 15 === 0) {
-      output.push("ping-pong");
-    } else if (i % 3 === 0) {
-      output.push("ping");
-    } else if (i % 5 === 0) {
-      output.push("pong");
-    } else  {
-      output.push(i);
-    }
-  }
-  return output;
+export function Entry() {
+function Entry(head, body) {
+ this.head = head;
+ this.body = body;
+}
 }
